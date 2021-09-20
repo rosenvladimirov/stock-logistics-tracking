4a4c47e9713401c2be1b0d05762a685f2be52e17
Rosen Vladimirov <vladimirov.rosen@gmail.cm>
2021-09-20 20:53:07 +0300

    init new repository

---
+++ stock_quant_expand/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    "name" : "",
+    "version" : "11.0.1.0",
+    "author" : "dXFactory Ltd.",
+    'category': 'Localization',
+    "description": """
+Add more informations on kanban view in package module
+    """,
+    'conflicts': [
+    ],
+    'depends': [
+        'stock',
+    ],
+    "demo" : [],
+    "data" : [
+              'views/stock_quant_views.xml',
+              ],
+    'license': 'AGPL-3',
+    "installable": True,
+}
